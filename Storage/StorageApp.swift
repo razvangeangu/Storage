a92f994ef8e7e289272d737f6baf869a0ba9b869
Razvan-Gabriel Geangu <razvan.geangu@gmail.com>
2026-06-05 14:47:20 +0100

    Initial Commit

---
+++ Storage/StorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorageApp.swift
+//  Storage
+//
+//  Created by Razvan-Gabriel Geangu on 05/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct StorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
